ad0a86b4bf4104adbaee28e4c79f565f56ad5f91
Vikas <vikas.kumar.singh1198@gmail.com>
2025-01-09 12:55:06 +0530

    reading adn writting file in nodejs

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,3 +1,9 @@
 const fs = require('fs');
 const textRead = fs.readFileSync('./txt/input.txt','utf-8');
 console.log(textRead);
+const textOut = `This is what I know about Avocado ${textRead}\n created on ${Date.now()}`;
+
+fs.writeFileSync('./txt/output.txt',textOut)
+console.log("file written");
+
+
